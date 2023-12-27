8decfd657b8983977d75d630835dbc246e9ac547
Davi <lobpreal@gmail.com>
2023-12-26 23:18:48 -0300

    projeto finalizado

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,17 @@
+import styles from './Home.module.css'
+import photo from '../../img/savings.svg'
+
 function contact() {
-  return <h1>Contact</h1>
+  return (
+    <div className={styles.home_container}>
+      <h1>Contact</h1>
+      <p>
+        Para entrar em contato comigo basta clicar em uma das redes sociais listadas abaixo da página!
+      </p>
+      <img src={photo} alt="Descrição da imagem" />
+
+    </div>
+  );
 }
 
 export default contact
